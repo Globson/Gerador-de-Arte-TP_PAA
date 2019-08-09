ec2e27cbb45e608fc23d8f5b237c529117033780
Globson <samuelpedro1999@gmail.com>
2019-08-08 21:28:03 -0300

    Corrigido menu

--- main.c
+++ main.c
@@ -8,34 +8,124 @@ int main(int argc, char const *argv[])
 {
     char Quadro[20][80];  //Matriz de caracteres para Obra de Arte.
     srand(time(NULL));
-    while(1){  //loop continuo do programa.
-        int a,b;   //Variaveis auxiliares.
-        inicia_quadro(Quadro);
-        menu();
-        printf("\nDigite a arte escolhida:");  //Menu interativo.
-        scanf("%d",&a);
-        printf("\nDigite a quantidade de figuras (menor ou igual a zero para aleatorio):");
-        scanf("%d",&b);
-        if(b<=0){   //Verificando se entrada deverá ser aleatoria.
-            b=1+(rand()%100);
-            printf("\n\t -> Quantidade aleatoria gerada: %d <-\n",b);
-        }
-        if(a==1){  //Arte de um Asterisco.
-          plota_asterisco(b,Quadro);
-        }
-        else if(a==2){  //Arte de simbolo de Soma.
-          plota_soma(b,Quadro);
-        }
-        else if(a==3){  //Arte de X.
-          plota_X(b,Quadro);
-        }
-        else if(a==4){ //Arte de simbolos misturados.
-          plota_aleatorio(b,Quadro);
-        }
-        else{  //Arte pessoal do aluno.
-          plota_obra_aluno(b,Quadro);
-        }
-        imprime_quadro(Quadro);  //Imprimindo arte gerada na tela.
+    int a,b;   //Variaveis auxiliares.
+    menu();
+    printf("\nDigite a arte escolhida:");  //Menu interativo.
+    scanf("%d",&a);
+    printf("\nDigite a quantidade de figuras (menor ou igual a zero para aleatorio):");
+    scanf("%d",&b);
+    if(b<=0){   //Verificando se entrada deverá ser aleatoria.
+        b=1+(rand()%100);
+        printf("\n\t -> Quantidade aleatoria gerada: %d <-\n",b);
     }
-    return 0;
+    if(a==1){  //Arte de um Asterisco.
+      while(1){
+        int c=3;  //Variavel auxiliar
+        inicia_quadro(Quadro);  //Incializando matriz de quadro.
+        plota_asterisco(b,Quadro); //Criando arte.
+        imprime_quadro(Quadro); //Imprimindo arte gerada na tela.
+        printf("\nDeseja fazer outro quadro com o mesmo valor?");
+        printf("\n\t0-Nao");
+        printf("\n\t1-Sim");
+        while(c!=1 && c!=0){   //Garantindo que entrada de opcao é valida.
+          printf("\nEntre:");
+          scanf("%d",&c);
+          if (c!=1 && c!=0) {
+            printf("\n\tValor invalido!\n");
+          }
+        }
+        if(c == 0){  //Caso o usuario opte por nao, programa descarta valores e finaliza.
+          break;
+        }
+      }
+    }
+    else if(a==2){  //Arte de simbolo de Soma.
+      while(1){
+        int c=3; //Variavel auxiliar
+        inicia_quadro(Quadro);   //Incializando matriz de quadro.
+        plota_soma(b,Quadro); //Criando arte.
+        imprime_quadro(Quadro);  //Imprimindo arte gerada na tela.
+        printf("\nDeseja fazer outro quadro com o mesmo valor?");
+        printf("\n\t0-Nao");
+        printf("\n\t1-Sim");
+        while(c!=1 && c!=0){   //Garantindo que entrada de opcao é valida.
+          printf("\nEntre:");
+          scanf("%d",&c);
+          if (c!=1 && c!=0) {
+            printf("\n\tValor invalido!\n");
+          }
+        }
+        if(c == 0){  //Caso o usuario opte por nao, programa descarta valores e finaliza.
+          break;
+        }
+      }
+    }
+    else if(a==3){  //Arte de X.
+      while(1){
+        int c=3; //Variavel auxiliar
+        inicia_quadro(Quadro);   //Incializando matriz de quadro.
+        plota_X(b,Quadro); //Criando arte.
+        imprime_quadro(Quadro);  //Imprimindo arte gerada na tela.
+        printf("\nDeseja fazer outro quadro com o mesmo valor?");
+        printf("\n\t0-Nao");
+        printf("\n\t1-Sim");
+        while(c!=1 && c!=0){  //Garantindo que entrada de opcao é valida.
+          printf("\nEntre:");
+          scanf("%d",&c);
+          if (c!=1 && c!=0) {
+            printf("\n\tValor invalido!\n");
+          }
+        }
+        if(c == 0){  //Caso o usuario opte por nao, programa descarta valores e finaliza.
+          break;
+        }
+      }
+    }
+    else if(a==4){ //Arte de simbolos misturados.
+      while(1){
+        int c=3; //Variavel auxiliar
+        inicia_quadro(Quadro);   //Incializando matriz de quadro.
+        plota_aleatorio(b,Quadro); //Criando arte.
+        imprime_quadro(Quadro);  //Imprimindo arte gerada na tela.
+        printf("\nDeseja fazer outro quadro com o mesmo valor?");
+        printf("\n\t0-Nao");
+        printf("\n\t1-Sim");
+        while(c!=1 && c!=0){   //Garantindo que entrada de opcao é valida.
+          printf("\nEntre:");
+          scanf("%d",&c);
+          if (c!=1 && c!=0) {
+            printf("\n\tValor invalido!\n");
+          }
+        }
+        if(c == 0){  //Caso o usuario opte por nao, programa descarta valores e finaliza.
+          break;
+        }
+      }
+    }
+    else{  //Arte pessoal do aluno.
+      while(1){
+        int c=3; //Variavel auxiliar
+        inicia_quadro(Quadro);  //Incializando matriz de quadro.
+        plota_obra_aluno(b,Quadro); //Criando arte.
+        imprime_quadro(Quadro);  //Imprimindo arte gerada na tela.
+        printf("\nDeseja fazer outro quadro com o mesmo valor?");
+        printf("\n\t0-Nao");
+        printf("\n\t1-Sim");
+        while(c!=1 && c!=0){    //Garantindo que entrada de opcao é valida.
+          printf("\nEntre:");
+          scanf("%d",&c);
+          if (c!=1 && c!=0) {
+            printf("\n\tValor invalido!\n");
+          }
+        }
+        if(c == 0){  //Caso o usuario opte por nao, programa descarta valores e finaliza.
+          break;
+        }
+      }
+    }
+  printf("\n\t=================================\n");
+  printf("\t    Desenvolvedor:\n");
+  printf("\t    Samuel Sena - 3494\n");
+  printf("\tOBRIGADO POR UTILIZAR O PROGRAMA!\n");
+  return 0;
 }
